e6494959db6a9f55a485c5473e1ab0018d56cfc1
Fabio Silveira <fabiosilveira@Fabios-MacBook-Pro.local>
2023-09-17 23:11:40 -0700

    Added Readme

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 // GET a tag by ID
 router.get("/:id", async (req, res) => {
   try {
-    const tagData = await Tag.findByPk(req.params.id, {
+    const tagData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
     if (!tagData) {
@@ -27,31 +27,31 @@ router.get("/:id", async (req, res) => {
     }
     res.status(200).json(tagData);
   } catch (err) {
-    res.status(500).json({ message: "Tag not found!" });
+    res.status(500).json({ message: "Category not found!" });
   }
 });
 
 // Create a new tag
 router.post("/", async (req, res) => {
   try {
-    const tagData = await Tag.create(req.body);
+    const tagData = await Category.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
-    res.status(400).json({ message: "Tag creation failed" });
+    res.status(400).json({ message: "Category creation failed" });
   }
 });
 
 // Update a tag by ID
 router.put("/:id", async (req, res) => {
   try {
-    const updated = await Tag.update(req.body, {
+    const updated = await Category.update(req.body, {
       where: { id: req.params.id },
     });
     !updated[0]
       ? res.status(404).json({ message: "No tag found with this id!" })
       : res.status(200).json(updated);
   } catch (err) {
-    res.status(500).json({ message: "Tag update failed" });
+    res.status(500).json({ message: "Category update failed" });
   }
 });
 
@@ -59,12 +59,12 @@ router.put("/:id", async (req, res) => {
 // Delete a tag by ID
 router.delete("/:id", async (req, res) => {
   try {
-    const deleted = await Tag.destroy({ where: { id: req.params.id } });
+    const deleted = await Category.destroy({ where: { id: req.params.id } });
     !deleted
       ? res.status(404).json({ message: "No tag found with this id!" })
       : res.status(200).json(deleted);
   } catch (err) {
-    res.status(500).json({ message: "Tag deletion failed" });
+    res.status(500).json({ message: "Category deletion failed" });
   }
 });
 
